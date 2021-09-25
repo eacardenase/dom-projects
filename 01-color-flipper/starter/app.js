0c5a5a13c615a72af8359e5581fd6615e99892f3
eacardenase <eacardenase@unal.edu.co>
2021-09-25 18:02:35 -0500

    hex page functionality

--- 01-color-flipper/starter/app.js
+++ 01-color-flipper/starter/app.js
@@ -1,4 +1,4 @@
-const colors = ["green", "red", "rgb(133, 122, 200)", "#f15025"];
+const colors = ["green", "red", "rgb(133, 122, 200)", "#f15025", "blue"];
 
 const btn = document.querySelector("#btn");
 const color = document.querySelector(".color");
@@ -6,7 +6,7 @@ const color = document.querySelector(".color");
 btn.addEventListener("click", function () {
     // get random number between 0 and 3
     const randomNumber = getRandomNumber();
-    console.log(randomNumber);
+    // console.log(randomNumber);
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
