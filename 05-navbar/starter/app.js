f328aa08112d529551efc8ebee7cb1df74d1c7d5
eacardenase <eacardenase@unal.edu.co>
2021-09-26 21:56:40 -0500

    toggle button final functionality

--- 05-navbar/starter/app.js
+++ 05-navbar/starter/app.js
@@ -1,10 +1,16 @@
 const links = document.querySelector(".links");
 const btn = document.querySelector(".nav-toggle");
 
+// My solution
+// btn.addEventListener("click", function () {
+//     if (links.classList.contains("show-links")) {
+//         links.classList.remove("show-links");
+//     } else {
+//         links.classList.add("show-links");
+//     }
+// });
+
+// solution given
 btn.addEventListener("click", function () {
-    if (links.classList.contains("show-links")) {
-        links.classList.remove("show-links");
-    } else {
-        links.classList.add("show-links");
-    }
+    links.classList.toggle("show-links");
 });
