4c496a50882ad11e4924d240e5759177842b50b5
eacardenase <eacardenase@unal.edu.co>
2021-09-29 19:42:27 -0500

    questions functionality by traversing the dom

---
+++ 08-questions/starter/app.js
@@ -0,0 +1,11 @@
+// traversing the dom
+
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (event) {
+        const question = event.currentTarget.parentElement.parentElement;
+        question.classList.toggle("show-text");
+        question.sib;
+    });
+});
