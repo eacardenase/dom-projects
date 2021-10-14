a1cf1abddf37293bfec72ca871f41321db3b5e21
Edwin Alexander Cárdenas <eacardenase@unal.edu.co>
2021-10-13 19:29:07 -0500

    slides finite approach

--- 15-slider/setup/app.js
+++ 15-slider/setup/app.js
@@ -20,13 +20,28 @@ prevBtn.addEventListener("click", function () {
 
 function carousel() {
     // working with slides
-    if (counter === slides.length) {
-        counter = 0;
-    } else if (counter < 0) {
-        counter = slides.length - 1;
+    // if (counter === slides.length) {
+    //     counter = 0;
+    // } else if (counter < 0) {
+    //     counter = slides.length - 1;
+    // }
+
+    // working with buttons
+    if (counter < slides.length - 1) {
+        nextBtn.style.display = "block";
+    } else {
+        nextBtn.style.display = "none";
+    }
+    if (counter > 0) {
+        prevBtn.style.display = "block";
+    } else {
+        prevBtn.style.display = "none";
     }
 
     slides.forEach(function (slide) {
         slide.style.transform = `translateX(-${counter * 100}%)`;
     });
 }
+
+// hidding the prev button when the page loads
+prevBtn.style.display = "none";
